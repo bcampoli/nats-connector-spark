5b880c61f09b29a98b374fbddc55d1f8310e73f0
Billy Campoli <william.campoli@logimethods.com>
2017-10-02 15:38:48 -0400

    made test run 100 times

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,5 +6,5 @@ nats-streaming-server -p 4223&
 
 for i in $(seq 0 100);
   do
-    mvn -Dtest=NatsStreamingToSparkTest#testNatsToKeyValueSparkConnectorWithAdditionalSubjects test &
+    mvn -Dtest=NatsStreamingToSparkTest#testNatsToKeyValueSparkConnectorWithAdditionalSubjects test 
 done

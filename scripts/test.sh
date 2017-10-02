91111aef96151821e5020e2f78b9a205dabd356e
Billy Campoli <william.campoli@logimethods.com>
2017-10-02 14:54:08 -0400

    changed to only 1 test

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@ set -e -x
 cd github-repo-master
 gnatsd -p 4221&
 nats-streaming-server -p 4223&
-mvn clean compile test
+mvn -Dtest=NatsStreamingToSparkTest#testNatsToKeyValueSparkConnectorWithAdditionalSubjects clean compile test
